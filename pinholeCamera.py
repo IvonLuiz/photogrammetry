656aa97cee80066389641631d6d9dd3b7d7bfa71
IvonLuiz <ivonluizgarcia@gmail.com>
2025-01-24 17:48:51 -0300

    plotting test and distortion

--- pinholeCamera.py
+++ pinholeCamera.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+import cv2
 
 class PinholeCamera():
     def __init__(self, K : np.ndarray, dist_coefs : np.ndarray, world_R_cam : np.ndarray, world_T_cam : np.ndarray):
@@ -29,7 +30,7 @@ class PinholeCamera():
     
     def project(self, world_pts_3d : np.ndarray) -> np.ndarray:
         """
-        Projects a 3d array of points into camera plane.
+        Projects a 3d array of points into camera plane considering lens distortions.
         
         Parameters:
         - world_pts_3d: Array of 3D points (Nx3).
@@ -41,7 +42,8 @@ class PinholeCamera():
         camera_matrix[:3, :3] = self.world_R_cam
         camera_matrix[:3, 3] = self.world_T_cam.flatten()
         
-        X = np.hstack((world_pts_3d, np.ones((world_pts_3d.shape[0], 1))))
+        X = np.ones((world_pts_3d.shape[0], 4))
+        X[:, :3] = world_pts_3d
         
         # Project into the image plane using the pinhole camera model:
         # x = K [R | T] X
@@ -55,41 +57,130 @@ class PinholeCamera():
         
         # Apply intrinsic matrix to get image coordinates
         img_coords_hom = self.K @ cam_coords[:3, :]  # (3xN)
-
+        
         # Normalize by the third row to get pixel coordinates
         img_pts_2d = (img_coords_hom[:2, :] / img_coords_hom[2, :]).T  # (Nx2)
-        print(img_coords_hom)
-        print(img_pts_2d)
-        return img_pts_2d
-    
-
         
+        # Apply lens distortion
+        k1, k2, p1, p2, k3 = self.dist_coefs  # Assuming distortion_coeffs is a list or array of 5 elements
+        x = img_pts_2d[:, 0]
+        y = img_pts_2d[:, 1]
+        r2 = x**2 + y**2
+        r4 = r2**2
+        r6 = r2**3
+
+        # Radial distortion
+        radial_distortion = 1 + k1 * r2 + k2 * r4 + k3 * r6
+
+        # Tangential distortion
+        x_tangential = 2 * p1 * x * y + p2 * (r2 + 2 * x**2)
+        y_tangential = p1 * (r2 + 2 * y**2) + 2 * p2 * x * y
+
+        # Apply distortions
+        x_distorted = x * radial_distortion + x_tangential
+        y_distorted = y * radial_distortion + y_tangential
+
+        img_pts_2d_distorted = np.vstack((x_distorted, y_distorted)).T
+
+        return img_pts_2d_distorted
+
+    
+    def unproject(self, image_pts_2d: np.ndarray) -> np.ndarray:
+        """
+        Projects 2D detected points from the image as camera rays.
+
+        Parameters:
+        - image_pts_2d: Array of 2D points (Nx2).
+
+        Returns:
+        - Array of 3D direction vectors in the camera coordinate system (Nx3).
+        """
+        # Undistort points
+        undistorted_pts = cv2.undistortPoints(image_pts_2d.reshape(-1, 1, 2), self.K, self.dist_coefs).reshape(-1, 2)
+
+        # Normalize 2D points using intrinsic matrix (K)
+        # Xc = K⁻¹ x Zc
+        K_inv = np.linalg.inv(self.K)
+        undistorted_pts_hom = np.hstack((undistorted_pts, np.ones((undistorted_pts.shape[0], 1))))
+        cam_rays = (K_inv @ undistorted_pts_hom.T).T
+
+        return cam_rays
+        
+    def apply_dist(self, img_pts_2d):
+        # Apply lens distortion
+        k1, k2, p1, p2, k3 = self.distortion_coeffs  # Assuming distortion_coeffs is a list or array of 5 elements
+        x = img_pts_2d[:, 0]
+        y = img_pts_2d[:, 1]
+        r2 = x**2 + y**2
+        r4 = r2**2
+        r6 = r2**3
+
+        # Radial distortion
+        radial_distortion = 1 + k1 * r2 + k2 * r4 + k3 * r6
+
+        # Tangential distortion
+        x_tangential = 2 * p1 * x * y + p2 * (r2 + 2 * x**2)
+        y_tangential = p1 * (r2 + 2 * y**2) + 2 * p2 * x * y
+
+        # Apply distortions
+        x_distorted = x * radial_distortion + x_tangential
+        y_distorted = y * radial_distortion + y_tangential
+
+        distorted_pts = np.vstack((x_distorted, y_distorted)).T
+        
+        return distorted_pts
+
         
 # TESTING
 def test_pinhole_camera():
-    # Intrinsic matrix
-    K = np.array([[1000, 0, 320],
-                  [0, 1000, 240],
+    # Camera configuration
+    K = np.array([[800, 0, 320],
+                  [0, 800, 240],
                   [0, 0, 1]])
-
-    # Distortion coefficients (not used in this test but included for completeness)
-    dist_coefs = np.zeros(5)
-
-    # Extrinsics (rotation and translation)
-    world_R_cam = np.eye(3)  # Identity matrix (no rotation)
-    world_T_cam = np.array([[0], [0], [-5]])  # Camera translated 5 units along Z-axis
-    print("world T cam", world_R_cam)
+    dist_coefs = np.array([0.1, -0.05, 0.001, 0.001, 0.0])  # Some distortion coefficients
+    world_R_cam = np.eye(3)
+    world_T_cam = np.array([[0], [0], [-10]])  # Translate the camera to -10 on the Z axis
     camera = PinholeCamera(K, dist_coefs, world_R_cam, world_T_cam)
-    world_pts_3d = np.array([[0, 0, 0],
-                             [1, 1, 0],
-                             [-1, -1, 0],
-                             [0.5, 0.5, 0],
-                             [-0.5, -0.5, 0]])
 
-    # Project the 3D points to the image plane
-    projected_pts = camera.project(world_pts_3d)
-    print("Projected Points:")
-    print(projected_pts)
+    # 3D Cube
+    cube_vertices = np.array([
+        [-1, -1, -1], [1, -1, -1], [1, 1, -1], [-1, 1, -1],
+        [-1, -1,  1], [1, -1,  1], [1, 1,  1], [-1, 1,  1]
+    ])
+    cube_edges = [
+        [0, 1], [1, 2], [2, 3], [3, 0],
+        [4, 5], [5, 6], [6, 7], [7, 4],
+        [0, 4], [1, 5], [2, 6], [3, 7]
+    ]
 
+    projected_pts = camera.project(cube_vertices)
+
+    # Plot
+    fig = plt.figure(figsize=(10, 5))
+    ax3d = fig.add_subplot(121, projection='3d')
+    ax2d = fig.add_subplot(122)
+
+    for edge in cube_edges:
+        ax3d.plot(
+            cube_vertices[edge, 0], cube_vertices[edge, 1], cube_vertices[edge, 2], color='blue'
+        )
+    ax3d.set_title("3D Cube")
+    ax3d.set_xlabel("X")
+    ax3d.set_ylabel("Y")
+    ax3d.set_zlabel("Z")
+
+    for edge in cube_edges:
+        start_idx, end_idx = edge
+        start_point = projected_pts[start_idx]
+        end_point = projected_pts[end_idx]
+        ax2d.plot([start_point[0], end_point[0]], [start_point[1], end_point[1]], color='red')
+    ax2d.set_title("2D Projection")
+    ax2d.set_xlabel("X")
+    ax2d.set_ylabel("Y")
+    ax2d.set_xlim(0, 640)
+    ax2d.set_ylim(480, 0)  # Inverter o eixo Y para coordenadas de imagem
+
+    plt.tight_layout()
+    plt.savefig("test pinhole camera")
 
 test_pinhole_camera()
