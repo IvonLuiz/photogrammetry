7dcaeb0264af7c1b6f6e9acda18b9a88f2222638
IvonLuiz <ivonluizgarcia@gmail.com>
2025-01-27 13:49:00 -0300

    class to estimate 3d coordinates from 2 set of 2d observations and 2 sets of camera parameters

---
+++ estimate3dCoord.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+
+class Triangulation:
+    def __init__(self, camera_params1, camera_params2):
+        """
+        Initialize the class with camera parameters for two cameras.
+        
+        :param camera_params1: Dictionary containing intrinsic and extrinsic parameters for camera 1.
+        :param camera_params2: Dictionary containing intrinsic and extrinsic parameters for camera 2.
+        """
+        self.camera_params1 = camera_params1
+        self.camera_params2 = camera_params2
+        
+        # Compute the projection matrices for both cameras
+        self.P1 = self._compute_projection_matrix(camera_params1)
+        self.P2 = self._compute_projection_matrix(camera_params2)
+    
+    def _compute_projection_matrix(self, camera_params):
+        """
+        Compute the projection matrix from camera parameters.
+        
+        :param camera_params: Dictionary containing intrinsic and extrinsic parameters.
+        :return: 3x4 projection matrix.
+        """
+        # Intrinsic matrix (K)
+        K = np.array(camera_params['intrinsic'])
+        
+        # Extrinsic parameters (rotation matrix R and translation vector t)
+        R = np.array(camera_params['rotation'])
+        t = np.array(camera_params['translation'])
+        
+        # Compute the extrinsic matrix [R | t]
+        extrinsic = np.hstack((R, t.reshape(-1, 1)))
+        
+        # Compute the projection matrix P = K * [R | t]
+        P = np.dot(K, extrinsic)
+        
+        return P
+    
+    def triangulate_points(self, points1, points2):
+        """
+        Triangulate 3D points from 2D correspondences in two images.
+        
+        :param points1: 2D points in the first image (Nx2 array).
+        :param points2: 2D points in the second image (Nx2 array).
+        :return: 3D points in world coordinates (Nx3 array).
+        """
+        points1_h = np.hstack((points1, np.ones((points1.shape[0], 1))))
+        points2_h = np.hstack((points2, np.ones((points2.shape[0], 1))))
+        
+        points_3d = np.zeros((points1.shape[0], 3))
+        
+        # Triangulate each pair of points
+        for i in range(points1.shape[0]):
+            x1 = points1_h[i, 0]
+            y1 = points1_h[i, 1]
+            x2 = points2_h[i, 0]
+            y2 = points2_h[i, 1]
+
+            A = np.vstack((
+                points1_h[i, 0] * self.P1[2, :] - self.P1[0, :],
+                points1_h[i, 1] * self.P1[2, :] - self.P1[1, :],
+                points2_h[i, 0] * self.P2[2, :] - self.P2[0, :],
+                points2_h[i, 1] * self.P2[2, :] - self.P2[1, :]
+            ))
+        
+            # Solving using SVD
+            _, _, Vt = np.linalg.svd(A)
+            X = Vt[-1, :4]  # Solution is the last row of Vt
+            
+            points_3d[i, :] = X[:3] / X[3]
+        
+        return points_3d
+
+if __name__ == "__main__":
+    # Define camera parameters (intrinsic and extrinsic)
+    camera_params1 = {
+        'intrinsic': np.array([[1000, 0, 320], [0, 1000, 240], [0, 0, 1]]),  # K matrix
+        'rotation': np.eye(3),  # R matrix (identity for simplicity)
+        'translation': np.array([0, 0, 0])  # t vector
+    }
+    
+    camera_params2 = {
+        'intrinsic': np.array([[1000, 0, 320], [0, 1000, 240], [0, 0, 1]]),  # K matrix
+        'rotation': np.eye(3),  # R matrix (identity for simplicity)
+        'translation': np.array([1, 0, 0])  # t vector (baseline of 1 unit)
+    }
+    
+    # Define 2D points in both images (Nx2 arrays)
+    points1 = np.array([[320, 240], [350, 260]])  # Example points in image 1
+    points2 = np.array([[320, 240], [340, 260]])  # Corresponding points in image 2
+    
+    # Create Triangulation object
+    triangulator = Triangulation(camera_params1, camera_params2)
+    
+    # Triangulate 3D points
+    points_3d = triangulator.triangulate_points(points1, points2)
+    
+    print("3D Points:\n", points_3d)
